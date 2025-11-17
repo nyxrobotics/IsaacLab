30163dbda02d8c3ab202823da3b6d10e1156e473
TakaakiNumai <takaaki.numai01@g.softbank.co.jp>
2025-11-17 15:41:22 +0900

    WIP

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/g1/kuroko_rough_low_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/g1/kuroko_rough_low_env_cfg.py
@@ -237,6 +237,7 @@ class KurokoRoughLowEnvCfg(LocomotionVelocityRoughEnvCfg):
         self.commands.base_velocity.ranges.lin_vel_x = (-0.4, 0.4)
         self.commands.base_velocity.ranges.lin_vel_y = (-0.4, 0.4)
         self.commands.base_velocity.ranges.ang_vel_z = (-4.0, 4.0)
+        self.commands.base_velocity.ranges.heading = (0.0, 0.0)
 
 
 # ---------------------------------------------------------------------
@@ -249,15 +250,23 @@ class KurokoRoughLowEnvCfg_PLAY(KurokoRoughLowEnvCfg):
     def __post_init__(self):
         super().__post_init__()
 
+        # make a smaller scene for play
         self.scene.num_envs = 50
         self.scene.env_spacing = 2.5
         self.episode_length_s = 40.0
 
+        # spawn the robot randomly in the grid (instead of their terrain levels)
+        self.scene.terrain.max_init_terrain_level = None
+        # reduce the number of terrains to save memory
         if self.scene.terrain.terrain_generator is not None:
             self.scene.terrain.terrain_generator.num_rows = 5
             self.scene.terrain.terrain_generator.num_cols = 5
             self.scene.terrain.terrain_generator.curriculum = False
 
+        self.scene.height_scanner = None
+        self.observations.policy.height_scan = None
+        # disable randomization for play
         self.observations.policy.enable_corruption = False
+        # remove random pushing
         self.events.base_external_force_torque = None
         self.events.push_robot = None

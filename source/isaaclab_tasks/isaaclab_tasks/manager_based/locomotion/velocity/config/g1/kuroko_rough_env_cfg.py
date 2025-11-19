5fd8a2c61a970df97e967855c146b63ea71cdd11
TakaakiNumai <takaaki.numai01@g.softbank.co.jp>
2025-11-19 15:28:28 +0900

    WIP

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/g1/kuroko_rough_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/g1/kuroko_rough_env_cfg.py
@@ -50,24 +50,24 @@ class KurokoRewards(RewardsCfg):
 
     termination_penalty = RewTerm(
         func=mdp.is_terminated,
-        weight=-200.0,
+        weight=-1000.0,
     )
 
     track_lin_vel_xy_exp = RewTerm(
         func=mdp.track_lin_vel_xy_yaw_frame_exp,
-        weight=1.0,
+        weight=8.0,
         params={"command_name": "base_velocity", "std": 0.5},
     )
 
     track_ang_vel_z_exp = RewTerm(
         func=mdp.track_ang_vel_z_world_exp,
-        weight=2.0,
+        weight=46.0,
         params={"command_name": "base_velocity", "std": 0.5},
     )
 
     joint_deviation_torso = RewTerm(
         func=mdp.joint_deviation_l1,
-        weight=-0.02,
+        weight=-0.001,
         params={"asset_cfg": SceneEntityCfg("robot", joint_names=["chest"])},
     )
 
@@ -85,7 +85,7 @@ class KurokoRewards(RewardsCfg):
 
     torso_height_limit = RewTerm(
         func=mdp.torso_height_limit,
-        weight= -1.0,
+        weight= -30.0,
         params={
             "asset_cfg": SceneEntityCfg(
                 "robot",
@@ -110,7 +110,7 @@ class KurokoRewards(RewardsCfg):
 
     joint_deviation_hip = RewTerm(
         func=mdp.joint_deviation_l1,
-        weight=-0.01,
+        weight=-0.001,
         params={"asset_cfg": SceneEntityCfg("robot", joint_names=[
                 "hip_l_pitch",
                 "hip_r_pitch"])},
@@ -118,7 +118,7 @@ class KurokoRewards(RewardsCfg):
 
     joint_deviation_arms = RewTerm(
         func=mdp.joint_deviation_l1,
-        weight=-0.1,
+        weight=-0.01,
         params={"asset_cfg": SceneEntityCfg("robot", joint_names=[
                 "shoulder_l_pitch",
                 "shoulder_l_roll",
@@ -299,7 +299,7 @@ class KurokoRoughEnvCfg(LocomotionVelocityRoughEnvCfg):
         self.rewards.dof_pos_limits = None
         self.rewards.undesired_contacts = None
         self.rewards.ang_vel_xy_l2 = None
-        self.rewards.flat_orientation_l2.weight = -10.0
+        self.rewards.flat_orientation_l2.weight = -40.0
         self.rewards.action_rate_l2.weight = -0.001
 
         self.rewards.dof_acc_l2.weight = -1.25e-7

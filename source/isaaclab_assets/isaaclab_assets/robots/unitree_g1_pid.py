999a16667836f621e6a7a4f4241139460e9b14aa
TakaakiNumai <takaaki.numai01@g.softbank.co.jp>
2026-02-20 11:58:19 +0900

    Disable PID

--- source/isaaclab_assets/isaaclab_assets/robots/unitree_g1_pid.py
+++ source/isaaclab_assets/isaaclab_assets/robots/unitree_g1_pid.py
@@ -15,7 +15,6 @@ Notes:
 
 from __future__ import annotations
 
-from isaaclab.actuators import ImplicitActuatorCfg
 from isaaclab.actuators import PIDActuatorCfg
 
 from .unitree import G1_MINIMAL_CFG  # isort: skip
@@ -116,74 +115,60 @@ G1_HANDS_PID_ACTUATOR_CFG = PIDActuatorCfg(
 G1_PID_CFG = G1_MINIMAL_CFG.copy()
 
 # Replace actuator setup with explicit PID controllers.
-G1_PID_CFG.actuators = {
-    # Legs
-    'hip':
-        G1_HIP_PID_ACTUATOR_CFG.replace(joint_names_expr=[
-            '.*_hip_yaw_joint',
-            '.*_hip_roll_joint',
-            '.*_hip_pitch_joint',
-        ],),
-    'knee':
-        G1_KNEE_PID_ACTUATOR_CFG.replace(joint_names_expr=[
-            '.*_knee_joint',
-        ],),
-    # Feet
-    'ankle_pitch':
-        G1_ANKLE_PITCH_PID_ACTUATOR_CFG.replace(joint_names_expr=[
-            '.*_ankle_pitch_joint',
-        ],),
-    'ankle_roll':
-        G1_ANKLE_ROLL_PID_ACTUATOR_CFG.replace(joint_names_expr=[
-            '.*_ankle_roll_joint',
-        ],),
-    # Waist
-    'waist_yaw':
-        G1_WAIST_YAW_PID_ACTUATOR_CFG.replace(joint_names_expr=[
-            'torso_joint',
-        ],),
-    # Arms / wrists
-    'arms':
-        G1_ARMS_PID_ACTUATOR_CFG.replace(joint_names_expr=[
-            '.*_shoulder_pitch_joint',
-            '.*_shoulder_roll_joint',
-            '.*_shoulder_yaw_joint',
-            '.*_elbow_pitch_joint',
-            '.*_elbow_roll_joint',
-        ],),
-    # Hands / fingers
-    # 'hands':
-    #     G1_HANDS_PID_ACTUATOR_CFG.replace(joint_names_expr=[
-    #         '.*_five_joint',
-    #         '.*_three_joint',
-    #         '.*_six_joint',
-    #         '.*_four_joint',
-    #         '.*_zero_joint',
-    #         '.*_one_joint',
-    #         '.*_two_joint',
-    #     ],),
-    'hands':
-        ImplicitActuatorCfg(
-            joint_names_expr=[
-                '.*_five_joint',
-                '.*_three_joint',
-                '.*_six_joint',
-                '.*_four_joint',
-                '.*_zero_joint',
-                '.*_one_joint',
-                '.*_two_joint',
-            ],
-            effort_limit_sim=300,
-            stiffness=40.0,
-            damping=10.0,
-            armature={
-                '.*_five_joint': 0.001,
-                '.*_three_joint': 0.001,
-                '.*_six_joint': 0.001,
-                '.*_four_joint': 0.001,
-                '.*_zero_joint': 0.001,
-                '.*_one_joint': 0.001,
-                '.*_two_joint': 0.001,
-            },
-        ),
-}
+
+# G1_PID_CFG.actuators = {
+#     'hip':
+#         G1_HIP_PID_ACTUATOR_CFG.replace(joint_names_expr=[
+#             '.*_hip_yaw_joint',
+#             '.*_hip_roll_joint',
+#             '.*_hip_pitch_joint',
+#         ],),
+#     'knee':
+#         G1_KNEE_PID_ACTUATOR_CFG.replace(joint_names_expr=[
+#             '.*_knee_joint',
+#         ],),
+#     'ankle_pitch':
+#         G1_ANKLE_PITCH_PID_ACTUATOR_CFG.replace(joint_names_expr=[
+#             '.*_ankle_pitch_joint',
+#         ],),
+#     'ankle_roll':
+#         G1_ANKLE_ROLL_PID_ACTUATOR_CFG.replace(joint_names_expr=[
+#             '.*_ankle_roll_joint',
+#         ],),
+#     'waist_yaw':
+#         G1_WAIST_YAW_PID_ACTUATOR_CFG.replace(joint_names_expr=[
+#             'torso_joint',
+#         ],),
+#     'arms':
+#         G1_ARMS_PID_ACTUATOR_CFG.replace(joint_names_expr=[
+#             '.*_shoulder_pitch_joint',
+#             '.*_shoulder_roll_joint',
+#             '.*_shoulder_yaw_joint',
+#             '.*_elbow_pitch_joint',
+#             '.*_elbow_roll_joint',
+#         ],),
+#     'hands':
+#         ImplicitActuatorCfg(
+#             joint_names_expr=[
+#                 '.*_five_joint',
+#                 '.*_three_joint',
+#                 '.*_six_joint',
+#                 '.*_four_joint',
+#                 '.*_zero_joint',
+#                 '.*_one_joint',
+#                 '.*_two_joint',
+#             ],
+#             effort_limit_sim=300,
+#             stiffness=40.0,
+#             damping=10.0,
+#             armature={
+#                 '.*_five_joint': 0.001,
+#                 '.*_three_joint': 0.001,
+#                 '.*_six_joint': 0.001,
+#                 '.*_four_joint': 0.001,
+#                 '.*_zero_joint': 0.001,
+#                 '.*_one_joint': 0.001,
+#                 '.*_two_joint': 0.001,
+#             },
+#         ),
+# }

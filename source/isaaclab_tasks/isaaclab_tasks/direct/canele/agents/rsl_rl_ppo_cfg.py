c31ea60826552dcc566874a6dd48e71f05c66d01
TakaakiNumai <takaaki.numai01@g.softbank.co.jp>
2026-03-10 14:44:57 +0900

    Fix directory name

--- source/isaaclab_tasks/isaaclab_tasks/direct/canele/agents/rsl_rl_ppo_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/direct/canele/agents/rsl_rl_ppo_cfg.py
@@ -13,7 +13,7 @@ class CaneleRoughPPORunnerCfg(RslRlOnPolicyRunnerCfg):
     num_steps_per_env = 24
     max_iterations = 3000
     save_interval = 50
-    experiment_name = "canele_rough"
+    experiment_name = "canele_direct"
     empirical_normalization = False
     policy = RslRlPpoActorCriticCfg(
         init_noise_std=1.0,
